0e1c6a2a77ba4ceabb828d7b9ea3fc09b8416c7a
karen <karen.aguayo@ucb.edu.bo>
2021-04-26 18:20:14 -0400

    primera prueba rspec

---
+++ bisiesto/lib/bisiesto.rb
@@ -0,0 +1,3 @@
+def EsBisiesto(num) 
+    true
+end
--- bisiesto/spec/EsBisiesto_spec.rb
+++ bisiesto/spec/EsBisiesto_spec.rb
@@ -1,5 +1,9 @@
+require './lib/bisiesto.rb'
+
 RSpec.describe "bisiesto" do
-    it "" do
-        
+    it "deberia devolver true para el anio 4" do
+        expect(EsBisiesto(4)).to eq(true)
     end
 end
+
+

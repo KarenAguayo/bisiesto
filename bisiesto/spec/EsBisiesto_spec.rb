9b79efdf0f10f0736f55264d437b38ac3c5877f0
karen <karen.aguayo@ucb.edu.bo>
2021-04-26 18:51:35 -0400

    cuarta prueba rspec

--- bisiesto/spec/EsBisiesto_spec.rb
+++ bisiesto/spec/EsBisiesto_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe "bisiesto" do
     it "deberia devolver true para el anio 400" do
         expect(EsBisiesto(400)).to eq(true)
     end
+    it "deberia devolver false para el anio 700" do
+        expect(EsBisiesto(700)).to eq(false)
+    end
 end
 
 
